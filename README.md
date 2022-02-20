de308a24527d79a68aaa065c1c8a0344f3654581
SWETRAK <kamilpietrak123@gmail.com>
2022-02-20 15:12:19 +0100

    After implementation

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # KPCheckBox
 
-A description of this package.
+Simple implementation of CheckBox for iOS devices.
+
+## Usage
+
+1. Copy url to Swift Package Manager 
+    
+2. Add to your project 
+
+3. Sample usage
+
+
+
--- Sources/KPCheckBox/KPCheckBox.swift
+++ Sources/KPCheckBox/KPCheckBox.swift
@@ -1,6 +1,26 @@
-public struct KPCheckBox {
-    public private(set) var text = "Hello, World!"
+#if !os(macOS)
+import SwiftUI
+import UIKit
 
-    public init() {
+@available(macOS 10.15, *)
+public struct KPCheckBox: View {
+    
+    @State var state: Bool
+    var callback: (_ newValue: Bool) -> Void
+    var size: Double = 22
+    var checkedColor: Color = Color(UIColor.systemBlue)
+    var uncheckedColor: Color = Color.secondary
+    
+    var body: some View {
+        Image(systemName: state ? "checkmark.square.fill" : "square")
+            .resizable()
+            .frame(width: self.size, height: self.size)
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(state ? self.checkedColor : self.uncheckedColor)
+            .onTapGesture {
+                self.state.toggle()
+                callback(self.state)
+            }
     }
 }
+#endif
